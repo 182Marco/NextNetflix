ca4b8cf7abe3c5a6bb8933ea3251061d3dfd4380
182Marco <marcomilza@gmail.com>
2022-11-29 08:50:13 +0100

    apiCalls.ts refactor

--- Utils/apiCalls.ts
+++ Utils/apiCalls.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
-import { API_URL, API_KEY } from "./constants"; 
+import { API_URL, API_KEY } from "./constants";
 import { MovieOrTvShow } from "./interfaces";
+import { en } from "./languages";
 
 // END POINTS
-export const trends = (type: string, lang: string) => `${API_URL}/${type}/popular?api_key=${API_KEY}&language=${lang}&page=1`;
+export const trendsEndPoint = (type: string, lang: string) =>
+  `${API_URL}/${type}/popular?api_key=${API_KEY}&language=${lang}&page=1`;
 
 // API CALLS
-export const getTrends = async (type: string, lang ='en-US') => {
-    try {
-       const res = await axios.get(trends(type, lang)) 
-       return res.data.results as MovieOrTvShow[];
-    } catch (er) {
-      console.error(`getTrends api responded with an error: ${er}`);
-    }
-    return [];
-  };
+export const getTrends = async (type = "tv", lang = en) => {
+  try {
+    const res = await axios.get(trendsEndPoint(type, lang));
+    return res.data.results as MovieOrTvShow[];
+  } catch (er) {
+    console.error(`getTrends api responded with an error: ${er}`);
+  }
+  return [];
+};
